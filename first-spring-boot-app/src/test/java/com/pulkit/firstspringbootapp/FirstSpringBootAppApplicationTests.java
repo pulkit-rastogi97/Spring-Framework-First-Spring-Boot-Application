58503680e141d18988413e83fa66b695cfc94475
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-06 02:36:43 +0530

    Spring Boot app generated

---
+++ first-spring-boot-app/src/test/java/com/pulkit/firstspringbootapp/FirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulkit.firstspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
