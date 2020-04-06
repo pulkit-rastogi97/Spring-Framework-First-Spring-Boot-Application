6de2e76a7c1492bdc6b4bb14d451333406c1a82f
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-06 17:45:01 +0530

    Test every unit through postman

--- first-spring-boot-app/src/main/java/com/pulkit/firstspringbootapp/models/Speaker.java
+++ first-spring-boot-app/src/main/java/com/pulkit/firstspringbootapp/models/Speaker.java
@@ -1,11 +1,14 @@
 package com.pulkit.firstspringbootapp.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Speaker {
 
     @Id
@@ -23,6 +26,7 @@ public class Speaker {
     private byte[] speaker_photo;
 
     @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
     private List<Session> sessions;
 
     public List<Session> getSessions() {
