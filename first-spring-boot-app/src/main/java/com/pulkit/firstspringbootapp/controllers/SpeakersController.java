e1cae5fb0185e1dbdfc1b27863360df5827f30c0
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-06 17:13:39 +0530

    Full CRUD Controller for RESTful Service of the resource Session

--- first-spring-boot-app/src/main/java/com/pulkit/firstspringbootapp/controllers/SpeakersController.java
+++ first-spring-boot-app/src/main/java/com/pulkit/firstspringbootapp/controllers/SpeakersController.java
@@ -1,9 +1,8 @@
 package com.pulkit.firstspringbootapp.controllers;
 
-import com.pulkit.firstspringbootapp.models.Session;
 import com.pulkit.firstspringbootapp.models.Speaker;
-import com.pulkit.firstspringbootapp.repositories.SessionRepository;
 import com.pulkit.firstspringbootapp.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,6 +24,27 @@ import java.util.List;
         @RequestMapping("{id}")
         public Speaker get(@PathVariable Long id){
             return speakerRepository.getOne(id);
+        }
+
+        @PostMapping
+        //@ResponseStatus(HttpStatus.CREATED)
+        public Speaker create(@RequestBody final Speaker speaker){
+            return speakerRepository.saveAndFlush(speaker);
+        }
+
+        @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+        public void delete(@PathVariable Long id){
+            //Also need to check for children records before deleting.
+            speakerRepository.deleteById(id);
+        }
+
+        @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+        public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+            //because this is a PUT, we expect all attributes to be passed in, A PATCH would only need what
+            //TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+            Speaker existingSpeaker = speakerRepository.getOne(id);
+            BeanUtils.copyProperties(speaker, speakerRepository, "speaker_id");
+            return speakerRepository.saveAndFlush(existingSpeaker);
         }
 
     }
