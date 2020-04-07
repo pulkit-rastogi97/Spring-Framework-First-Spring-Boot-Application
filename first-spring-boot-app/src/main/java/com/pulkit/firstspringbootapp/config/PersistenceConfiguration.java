b2cb169c1c29a0b228f1dc517c1fc130d7d1f60e
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-08 00:38:22 +0530

    Custom Configuration through Java Classes without properties file

---
+++ first-spring-boot-app/src/main/java/com/pulkit/firstspringbootapp/config/PersistenceConfiguration.java
@@ -0,0 +1,22 @@
+package com.pulkit.firstspringbootapp.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/FirstSpringBootApp");
+        builder.username("postgres");
+        builder.password("Welcome");
+        System.out.println("My Custom dataSource bean has been initialized and set ");
+
+        return builder.build();
+    }
+}
